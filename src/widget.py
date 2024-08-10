2c34d31ba91c62290e4ca0a18d4edafbcc55516d
Andrey Sarason <sarason@rambler.ru>
2024-08-11 02:13:45 +0300

    corr syntax in widget

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,6 @@
-def mask_account_car(card_type: str, card_number: str, account: str, account_numbers: str) -> str:
+def mask_account_card(card_type: str, card_number: str, account: str, account_numbers: str) -> str:
     """Функция  которая умеет обрабатывать информацию о картах и о счетах"""
-    if mask_account_car == card_type:
+    if mask_account_card == card_type:
         return f"{card_type[:]} {card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
 
     else:
