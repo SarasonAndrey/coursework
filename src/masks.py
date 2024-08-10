153400a31023787e9e6b99235d24eb958ae48e03
Andrey Sarason <sarason@rambler.ru>
2024-08-11 02:06:47 +0300

    modified file masks and widget

--- src/masks.py
+++ src/masks.py
@@ -2,26 +2,17 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция принимает номер карты 12 цифр и возвращает ее маску
     :rtype: object
     """
-    return (
-            card_number[:4]
-            + " "
-            + card_number[4:6]
-            + "*" * len(card_number[7:9])
-            + " "
-            + "*" * len(card_number[8:12])
-            + " "
-            + card_number[-4:]
-    )
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_mask_card_number("7000792289606361"))
 
 
 def get_mask_account(account: str) -> str:
     """Функция принимает номер счета 20 цифр и возвращает ее маску"""
-    return "*" * len(account[-7:-5]) + account[-4:]
+    return f"**{account[-4:]}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -8,4 +8,6 @@ def mask_account_car(card_type: str, card_number: str, account: str, account_num
 
 
 def get_date(data: str) -> str:
+    """Функция принимает на вход строку с датой в формате 2024-03-11T02:26:18.671407
+ и возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     return f"{data[8:10]}.{data[5:7]}.{data[:4]}"
