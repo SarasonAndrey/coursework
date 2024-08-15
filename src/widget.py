7348686436b44cf5f25310abcb9f171870ab1f18
Andrey Sarason <sarason@rambler.ru>
2024-08-15 20:28:49 +0300

    add .gitignore + .idea/ and  modified file src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,30 @@
-def mask_account_card(card_type: str, card_number: str, account: str, account_numbers: str) -> str:
-    """Функция  которая умеет обрабатывать информацию о картах и о счетах"""
-    if mask_account_card == card_type:
-        return f"{card_type[:]} {card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(name: str) -> str | None:
+    """Функция  которая скрывает информацию о картах и счетах"""
+    if "Счет" in name:
+        card = get_mask_account(name[-20:])
+        new_card = name.replace(name[-20:], card)
+
+        return new_card
 
     else:
-        return f"{account[:]} **{account_numbers[-4:]}"
+        card = get_mask_card_number(name[-16:])
+        new_card = name.replace(name[-16:], card)
+
+        return new_card
 
 
-def get_date(data: str) -> str:
-    """Функция принимает на вход строку с датой в формате 2024-03-11T02:26:18.671407
- и возвращает строку с датой в формате ДД.ММ.ГГГГ"""
-    return f"{data[8:10]}.{data[5:7]}.{data[:4]}"
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Visa Gold 5999414228426353"))
+
+if __name__ == "__main__":
+    def get_date(data: str) -> str | None:
+        """Функция принимает на вход строку с датой в формате 2024-03-11T02:26:18.671407
+            и возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+        return f"{data[8:10]}.{data[5:7]}.{data[:4]}"
+
+if __name__ == "__main__":
+    print(get_date("2024-03-11T02:26:18.671407"))
