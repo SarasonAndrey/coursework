a30728fc1ef9aafb4739861b588dbd94edeee275
Andrey Sarason <sarason@rambler.ru>
2024-09-07 17:31:47 +0300

    cor test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,6 +2,6 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_masks():
-    assert get_mask_card_number("7000792289606361") == '7000 79** **** 6361'
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
-    assert get_mask_account("73654108430135874305") == "'** 4305'"
+    assert get_mask_account("73654108430135874305") == "** 4305"
