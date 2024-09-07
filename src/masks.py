64a518ad8954e9def6f14dc54da4d9fd6088cac0
Andrey Sarason <sarason@rambler.ru>
2024-09-07 17:34:20 +0300

    cor test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -5,14 +5,11 @@ def get_mask_card_number(card_number: str) -> str:
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number("7000792289606361"))
-
-
 def get_mask_account(account: str) -> str:
     """Функция принимает номер счета 20 цифр и возвращает ее маску"""
     return f"**{account[-4:]}"
 
 
 if __name__ == "__main__":
+    print(get_mask_card_number("7000792289606361"))
     print(get_mask_account("73654108430135874305"))
