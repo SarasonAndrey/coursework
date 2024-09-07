1b358e23ca6dd356c0a1a076b06b6002de79a44f
Andrey Sarason <sarason@rambler.ru>
2024-09-07 15:43:15 +0300

    add tests file

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
---
+++ tests/test_widget.py
@@ -0,0 +1,9 @@
+from src.widget import mask_account_card, get_date
+
+
+def test_masks():
+    assert mask_account_card("Счет 64686473678894779589") == 'Счет **9589'
+    assert mask_account_card("MasterCard 7158300734726758") == 'MasterCard 7158 30** **** 6758'
+    assert mask_account_card("Visa Gold 5999414228426353") == 'Visa Gold 5999 41** **** 6353'
+
+    assert get_date("2024-03-11T02:26:18.671407") == '11.03.2024'
