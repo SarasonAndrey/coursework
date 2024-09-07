6f2357df017d3a7f6c640be4448d6b756d0c28ae
Andrey Sarason <sarason@rambler.ru>
2024-09-07 17:54:43 +0300

    modifie file

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,4 +4,5 @@ from src.masks import get_mask_card_number, get_mask_account
 def test_masks():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
-    assert get_mask_account("73654108430135874305") == "** 4305"
+    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("135874305") == "**4305"
