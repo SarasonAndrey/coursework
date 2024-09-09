8be80831f0a2fc5fd10b8a9de099128181cabfdb
Andrey Sarason <sarason@rambler.ru>
2024-09-09 18:07:15 +0300

    The test functions have been edited

--- src/processing.py
+++ src/processing.py
@@ -22,14 +22,14 @@ def filter_by_state(data: list[dict[str, Any]], state: str = "EXECUTED") -> list
 
 
 def sort_by_date(
-        list_of_dict: list[dict[str, Any]], reverse: bool = True
+        list_of_dicts: list[dict[str, Any]], reverse: bool = True
 ) -> list[dict[str, Any]]:
     """
     Функция принимает на вход список словарей и возвращает новый список, в котором исходные
     словари отсортированы по убыванию даты
     """
     sorted_list = sorted(
-        list_of_dict,
+        list_of_dicts,
         key=lambda new_list_of_dict: new_list_of_dict["date"],
         reverse=reverse,
     )
@@ -39,9 +39,4 @@ def sort_by_date(
 if __name__ == "__main__":
     print(filter_by_state(list_of_dicts))
 
-    print([
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ])
+    print((sort_by_date(list_of_dicts)))
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,8 +1,10 @@
+import pytest
+
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def test_masks():
+def test_mask():
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
+def test_mask():
     assert get_mask_account("73654108430135874305") == "**4305"
-    assert get_mask_account("135874305") == "**4305"
