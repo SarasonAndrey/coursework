08b727a0b988062063679175f222c238658c2a0f
Andrey Sarason <sarason@rambler.ru>
2024-09-09 20:02:59 +0300

    cor conftest

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,9 +1,9 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_mask() -> None:
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+def test_mask(number) -> None:
+    assert get_mask_card_number("7000792289606361") == number
 
 
-def test_mask_() -> None:
-    assert get_mask_account("73654108430135874305") == "**4305"
+def test_mask_(account) -> None:
+    assert get_mask_account("73654108430135874305") == account
