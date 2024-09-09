eba36a9e84d18ab99ccb40c03a6b85ac4ee1adc4
Andrey Sarason <sarason@rambler.ru>
2024-09-09 18:19:08 +0300

    work on bugs

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,7 +1,7 @@
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_masks():
+def test_masks() -> None:
     assert filter_by_state([
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -11,7 +11,7 @@ def test_masks():
            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
 
 
-def test_masks():
+def test_masks_() -> None:
     assert sort_by_date([
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,7 @@
 from src.widget import mask_account_card, get_date
 
-def test_masks():
+
+def test_masks() -> None:
     assert mask_account_card("Счет 64686473678894779589") == 'Счет **9589'
     assert mask_account_card("MasterCard 7158300734726758") == 'MasterCard 7158 30** **** 6758'
     assert mask_account_card("Visa Gold 5999414228426353") == 'Visa Gold 5999 41** **** 6353'
