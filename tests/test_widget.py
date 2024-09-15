3fd955005a7da68f8832dcb386500de682a5ef28
Andrey Sarason <sarason@rambler.ru>
2024-09-15 16:35:55 +0300

    modified:   tests/test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,16 +1,23 @@
+from __future__ import annotations
+
 from typing import Any
+
+import pytest
 
 from src.widget import get_date, mask_account_card
 
 
-def test_masks_account(account: Any) -> Any:
-    assert mask_account_card(account) == 'Счет **4305'
+@pytest.mark.parametrize('number, expected_result', [
+    ('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+    ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
+    ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
+    ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
+    ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
+    ('Visa Gold 5999414228426353', 'Visa Gold 5999 41** **** 6353'),
+])
+def test_widget(number: Any, expected_result: Any) -> Any:
+    assert mask_account_card(number) == expected_result
 
 
-def test_masks_number(number: Any) -> Any:
-    assert mask_account_card(number) == 'Visa Platinum 7000 79** **** 6361'
-
-
-
-def test_masks_() -> Any:
+def test_widget_data() -> Any:
     assert get_date("2024-03-11T02:26:18.671407") == '11.03.2024'
