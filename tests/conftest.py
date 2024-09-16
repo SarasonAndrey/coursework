7c4d4c4a7445c4c045c4572d33bb7a430d509fcc
Andrey Sarason <sarason@rambler.ru>
2024-09-16 19:47:03 +0300

    modified:   tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,6 @@ import pytest
 
 
 @pytest.fixture
-def number() -> Any:
-    return '7000792289606361'
-    return '1596837868705199'
-    return '7158300734726758'
-    return '6831982476737658'
-    return '8990922113665229'
-    return '5999414228426353'
-
-
-@pytest.fixture
 def account() -> Any:
     return '73654108430135874305'
 
