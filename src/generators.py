a6f27d08eebca2f72e5361373f87eb2c7b92c1f4
Andrey Sarason <sarason@rambler.ru>
2024-09-18 15:57:12 +0300

    modified:   src/generators.p, add function filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,6 @@
-list_of_code = {
+from typing import Any
+
+transactions = {
     "id": 939719570,
     "state": "EXECUTED",
     "date": "2018-06-30T02:08:58.425572",
@@ -12,16 +14,32 @@ list_of_code = {
     "description": "Перевод организации",
     "from": "Счет 75106830613657916952",
     "to": "Счет 11776614605963066702"
-},
+}, {
+    "id": 939719570,
+    "state": "EXECUTED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {
+        "amount": "9824.07",
+        "currency": {
+            "name": "USS",
+            "code": "USS"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702"
+}
 
 
-def filter_by_currency(transactions: list[dict], code: str = "USD"):
-    list_of_code = []
-    for values in code:
-        if values.get("code") == code:
-            list_of_code.append(values)
-    return list_of_code
+def filter_by_currency(data: list[dict[str, Any]], code: str = "USS") -> list[dict[str, Any]]:
+    """Функция выдает список трансакций с определенной валютой"""
+
+    list_of_data = []
+    for values in data:
+        if values.get("operationAmount").get("currency").get("code") == code:
+            list_of_data.append(values)
+    return list_of_data
 
 
 if __name__ == "__main__":
-    print(filter_by_currency(list_of_code))
+    print(filter_by_currency(transactions))
