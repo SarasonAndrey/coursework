9cfa346bb5546a3c8919096af0af347742ef86ba
Andrey Sarason <sarason@rambler.ru>
2024-09-19 11:04:40 +0300

    modified:   src/generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,46 +1,41 @@
 from typing import Any
 
-transactions = [{
-    "id": 939719570,
-    "state": "EXECUTED",
-    "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-        "amount": "9824.07",
-        "currency": {
-            "name": "USD",
-            "code": "USD"
-        }
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
     },
-    "description": "Перевод организации",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
-},
     {
         "id": 142264268,
         "state": "EXECUTED",
         "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {
-                "name": "USS",
-                "code": "USS"
-            }
-        },
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод со счета на счет",
         "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
-    }]
+        "to": "Счет 75651667383060284188",
+    },
+]
 
 
-def filter_by_currency(data: list[dict[str, Any]], code: str = "USD") -> list[dict[str, Any]]:
+def filter_by_currency(data: list[dict[str, Any]], code: str | None = "USD") -> list[dict[str, Any]] | None:
     """Функция выдает список трансакций с определенной валютой"""
 
     list_of_data = []
     for values in data:
         if values.get("operationAmount").get("currency").get("code") == code:
             list_of_data.append(values)
-    return list_of_data
+    yield list_of_data
+
+
+def transaction_descriptions():
+    pass
 
 
 if __name__ == "__main__":
-    print(filter_by_currency(transactions))
+    for _ in range(2):
+        print(next(filter_by_currency(transactions)))
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,14 @@ import pytest
 
 @pytest.fixture
 def account() -> Any:
-    return '73654108430135874305'
+    return "73654108430135874305"
 
 
 @pytest.fixture
 def account1() -> Any:
-    return '64686473678894779589'
+    return "64686473678894779589"
 
 
 @pytest.fixture
 def account2() -> Any:
-    return '35383033474447895560'
+    return "35383033474447895560"
