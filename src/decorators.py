6a464db9e8f4eac756829f3f4bd295f19841e077
Andrey Sarason <sarason@rambler.ru>
2024-09-22 16:06:18 +0300

    modified:   src/decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,37 @@
-from typing import Any
+def log(filename=None):
+    """Функция регистрирует детали выполнения функций"""
+
+    def decorator(my_func):
+
+        def wrapper(*args, **kwargs):
+            if not filename:
+                print(f"{my_func.__name__} started")
+                try:
+                    my_func(*args, **kwargs)
+                    print(f"{my_func.__name__} ok")
+                    print(f"{my_func.__name__} finished")
+                except Exception as e:
+                    print(f"{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+            else:
+                try:
+                    my_func(*args, **kwargs)
+                    with open(filename, "w") as file:
+                        file.write(f"{my_func.__name__} ok")
+                except Exception as e:
+                    with open(filename, "w") as file:
+                        file.write(
+                            f"{my_func.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                        )
+            # return result
+
+        return wrapper
+
+    return decorator
 
 
-def log(filename: Any | None = None) -> None:
-    """Декоратор регистрирует детали выполнения функций"""
-
-    def wrapper(func):
-        def inner(*args, **kwargs):
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
 
 
+my_function(1,6)
