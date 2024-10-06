80f3522cb5e24e91f368b90dd7aa7a9721329f1c
Andrey Sarason <sarason@rambler.ru>
2024-10-06 15:46:14 +0300

    modified:src/external_api.py

---
+++ src/external_api.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+
+def transaction_amount(transaction: dict) -> float:
+    """Функция конвертации"""
+    amount = transaction["operationAmount"]["amount"]
+    code = transaction["operationAmount"]["currency"]["code"]
+    if transaction["operationAmount"]["currency"]["code"] != "RUB":
+        response = requests.get(
+            f"https://api.apilayer.com/exchangerates_data/convert?to={"RUB"}&from={code}&amount={amount}",
+            headers={"apikey": f"{API_KEY}"},
+        )
+        result = response.json()
+        return result["result"]
+    else:
+        return float(amount)
+
+
